dc8803093f9ea5a189b33b5295d806858b64b9f9
Muhammad Hannan Hasan Ali <muhammadhannanhasanali@outlook.com>
2023-11-27 01:16:01 +0500

    done

--- HospitalERP.Web/wwwroot/js/site.js
+++ HospitalERP.Web/wwwroot/js/site.js
@@ -12,4 +12,30 @@
         $('.hidden-buttons').addClass('hidden');
     });
     $('[data-toggle="tooltip"]').tooltip();
+
+    /*card search*/
+    $("#search").on("input", function () {
+        var value = $(this).val().toLowerCase();
+        var column = $("#searchColumn").val();
+
+        if (column === null) {
+            column = "Name";
+        }
+
+        $(".searchit").filter(function () {
+            var cardId = $(this).find("#" + column).text().toLowerCase();
+            $(this).toggle(cardId.includes(value));
+        });
+    });
+
+    /*three dots into menu*/
+    $(".options").click(function () {
+        var itemId = $(this).data("item-id");
+
+        // Hide all other options before showing the current one
+        $(".button-section").not("[data-item-id='" + itemId + "']").addClass("hidden");
+
+        // Toggle visibility for options of the clicked card
+        $(".button-section[data-item-id='" + itemId + "']").toggleClass("hidden");
+    });
 });
